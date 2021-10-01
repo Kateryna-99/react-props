a44c6193b8343dd63f423d2bc3439dc1a11ed1c0
Katy <78986968+Kateryna-99@users.noreply.github.com>
2021-10-02 02:24:12 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import { render } from 'enzyme';
 import React from 'react';
 import './App.css';
 
+
 export default function App() {
+  
+    someList={[  [{id: 100, text: 'Animals'},
+    {id: 101, text: 'Anime'},
+    {id: 102, text: 'Anti-Malware'},
+    {id: 103, text: 'Art Design'},
+    {id: 104, text: 'Books'},
+    {id: 105, text: 'Business'},
+    {id: 106, text: 'Calendar'},
+    {id: 107, text: 'Cloud Storage'},
+    {id: 108, text: 'File Sharing'},
+    {id: 109, text: 'Animals'},
+    {id: 110, text: 'Continuous Integration'},
+    {id: 111, text: 'Cryptocurrency'}];
+  }}
+    render() 
   return (
-    <div>
-      Some data:
+    <div> 
+      Some List:
+      <ul>
+       {state.someList.map(elm => (<li key={elm.id}>{elm.text}</li>))}
+      </ul>
     </div>
-  );
+  )
+;
+
+class FirstElem extends React.Component {
+  render(){
+    return <li>{this.props.item.text}</li>
+  }
 }
+  
